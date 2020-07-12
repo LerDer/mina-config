4d387df1dec5d9a2a926ba3548f94dd0f62e1373
lww <lerder.liu@shebaochina.com>
2020-07-13 01:17:31 +0800

    获取单个配置信息

--- mina-server/src/main/java/com/lww/mina/controller/MessageController.java
+++ mina-server/src/main/java/com/lww/mina/controller/MessageController.java
@@ -1,5 +1,7 @@
 package com.lww.mina.controller;
 
+import com.baomidou.mybatisplus.core.conditions.query.QueryWrapper;
+import com.lww.mina.domain.Message;
 import com.lww.mina.result.HttpResult;
 import com.lww.mina.service.MessageService;
 import com.lww.mina.vo.MessageVO;
@@ -11,6 +13,7 @@ import javax.annotation.Resource;
 import lombok.extern.slf4j.Slf4j;
 import org.apache.commons.lang3.StringUtils;
 import org.springframework.util.Assert;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -56,4 +59,20 @@ public class MessageController {
         return HttpResult.success();
     }
 
+    @ApiOperation("获取单个配置信息")
+    @ApiImplicitParams({
+            @ApiImplicitParam(name = "projectName", value = "项目名称", required = true),
+            @ApiImplicitParam(name = "env", value = "环境", required = true),
+            @ApiImplicitParam(name = "propertyValue", value = "application.properties 配置的值", required = true),
+    })
+    @GetMapping(value = "/conf", name = "获取配置信息")
+    public Message conf(String projectName, String env, String propertyValue) {
+        Message message = messageService.getOne(new QueryWrapper<Message>().lambda()
+                .eq(Message::getProjectName, projectName)
+                .eq(Message::getEnvValue, env)
+                .eq(Message::getPropertyValue, propertyValue)
+                .eq(Message::getIsDeleted, 0));
+        Assert.isTrue(message != null, "查询结果为空！");
+        return message;
+    }
 }
